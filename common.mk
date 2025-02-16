crackedpotato007/android_device_oneplus_benz

sm8550-common: Update audio-HAL path
It has been moved to a subdir and the structure is now as follows:

  hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio
  ├─ agm (clo:platform/vendor/qcom/opensource/agm)
  ├─ pal (clo:platform/vendor/qcom/opensource/arpal-lx)
  └─ primary_hal (clo:platform/hardware/qcom/audio-ar)

Change-Id: Icfce2884a482b498e97f931732d37974161cf620

common.mk
@@ -66,7 +66,7 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     sound_trigger.primary.kalama
 
-AUDIO_HAL_DIR := hardware/qcom-caf/sm8550/audio
+AUDIO_HAL_DIR := hardware/qcom-caf/sm8550/audio/primary-hal
 
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \

pjgowtham/android_device_oplus_sm8450-common

common.mk
@@ -58,7 +58,7 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     sound_trigger.primary.taro:32
 
-AUDIO_HAL_DIR := hardware/qcom-caf/sm8450/audio
+AUDIO_HAL_DIR := hardware/qcom-caf/sm8450/audio/primary-hal
 QCV_FAMILY_SKUS := cape taro
 
 PRODUCT_COPY_FILES += \